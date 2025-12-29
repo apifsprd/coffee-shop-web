7033f69502b4f0870ac1892d3f2920274548b9f6
Apif Supriadi <apif.supriadi@uiii.ac.id>
2025-12-29 16:06:58 +0700

    feat(landing): add benefits section
    - Implemented new benefits section
    - Added icons and descriptions for features
    - Included a background image for the section
    - Adjusted overall page layout
    - Refined typography for better readability

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 // import { Geist, Geist_Mono } from "next/font/google";
 
 import { ButtonCTA, ButtonLink } from "@/components/ui/Button";
+import { Coffee, HeartHandshake, IdCard } from "lucide-react";
 import Image from "next/image";
 
 // const geistSans = Geist({
@@ -13,11 +14,29 @@ import Image from "next/image";
 //   subsets: ["latin"],
 // });
 
+const benefits = [
+  {
+    title: "Exceptional Coffee",
+    description: "Better productivity and businees starts and ends with better coffee. If you love great coffee you're going to love Indo Cafe n Resto.",
+    icon: <Coffee className="w-16 h-16" />,
+  },
+  {
+    title: "Flexible Co-Working",
+    description: "Our co-working space offers a variety of options for both members and non-members alike. We created a space where anyone can do better business.",
+    icon: <HeartHandshake className="w-16 h-16" />,
+  },
+  {
+    title: "Affordable Membership",
+    description: "We offer a wide range of membership options to suit every budget. Whether you're a student, a professional, or a business, we have a membership plan that meets your needs.",
+    icon: <IdCard className="w-16 h-16" />,
+  }
+]
+
 export default function Home() {
   return (
-    <div className="flex flex-col gap-8 pt-4 pb-16 px-16 min-h-screen w-full">
+    <div className="flex flex-col gap-8 pt-4 pb-16 px-0 min-h-screen w-full">
       <header>
-        <nav className="flex flex-row items-center justify-start relative px-16">
+        <nav className="flex flex-row items-center justify-start relative px-32">
           <div className="w-20 h-20 object-contain rounded-lg ">
             <Image src="/images/logo.png" alt="Logo" width={100} height={100} />
           </div>
@@ -44,7 +63,7 @@ export default function Home() {
         </nav>
       </header>
       <main className="flex flex-col gap-16">
-        <section className="flex flex-row items-center justify-between py-8 px-16  bg-gray-100 rounded-2xl">
+        <section className="flex flex-row items-center justify-between py-8 px-16 mx-16  bg-gray-100 rounded-2xl">
           <div className="flex flex-col gap-8 flex-1 px-44">
             <h1 className="text-7xl font-bold">
               Where Good Coffee and Great Ideas meet!
@@ -79,7 +98,7 @@ export default function Home() {
             <h3 className="text-4xl font-bold">
               Where We Do Business — Indo Cafe n Resto
             </h3>
-            <h5 className="text-2xl">
+            <h5 className="text-lg">
               Indo Cafe n Resto aims to create a flexible co-working environment
               that provides solutions to a wide variety of working
               professionals, The best place to do bussines with the best coffee
@@ -107,6 +126,31 @@ export default function Home() {
               </div>
             </div>
           </div>
+        </section>
+        <section className="w-full h-auto p-4 relative overflow-hidden"> 
+       <div 
+    className="absolute inset-0 bg-[url('/images/landing-page/coffee-shop-landscape.jpg')] bg-cover bg-center">
+  </div>
+
+  <div className="absolute inset-0 bg-black/80"></div>
+
+  <div className="relative z-10 flex flex-col h-full items-center justify-center gap-4 m-16">
+    <p className="text-base text-white uppercase">Indo Cafe n Resto Benefits</p>
+   <h3 className="text-4xl font-bold text-white"> The Cozy Cafe Experience</h3>
+
+   <div className="grid grid-cols-1 gap-4 lg:grid-cols-3 w-full mt-4">
+   {benefits.map((benefit, index) => (
+      <div key={index} className="p-8 rounded-2xl bg-white col-span-1 flex flex-col gap-4 justify-startz items-center">
+      {benefit.icon}
+      <div className="h-16 flex flex-col justify-center items-center">
+        <h5 className="text-2xl text-center">{benefit.title}</h5>
+      </div>
+      <p className="text-base text-center text-gray-500 leading-5">{benefit.description}</p>
+    </div>
+   ))}
+    
+   </div>
+  </div>
         </section>
       </main>
     </div>
